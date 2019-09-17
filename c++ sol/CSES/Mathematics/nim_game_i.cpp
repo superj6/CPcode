8355994db25a60d10676baa0a5ad693327c83424
superj6 <46981561+superj6@users.noreply.github.com>
2019-09-17 14:56:43 -0500

    Add files via upload

---
+++ c++ sol/CSES/Mathematics/nim_game_i.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+
+void answer(){
+        int n;
+        cin >> n;
+
+   int ret = 0;
+   for(int i = 0; i < n; i++){
+       int x;
+       cin >> x;
+
+      ret ^= x;
+   }
+
+   cout << (ret ? "first" : "second") << endl;
+}
+
+int main() {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+
+   int t;
+   cin >> t;
+
+   for(int i = 0; i < t; i++) answer();
+
+        return 0;
+}
