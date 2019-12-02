1ce47c54bf8656e083a1c9431bacaf7c67f3fdaa
superj6 <46981561+superj6@users.noreply.github.com>
2019-12-01 16:53:38 -0800

    Update seedsPRING.cpp

--- CTF/Pico19/BinExp/seedsPRING.cpp
+++ CTF/Pico19/BinExp/seedsPRING.cpp
@@ -1,1 +1,14 @@
-https://github.com/superj6/CPcode/raw/master/CTF/Pico19/BinExp/seedsPING.cpp
+#include <iostream>
+#include <random>
+#include <time.h>
+using namespace std;
+#define endl '\n'
+ 
+int main(){
+	srand(time(0));
+ 
+	int n = 30;
+	for(int i = 0; i < n; i++){
+		cout << (rand() & 0xf) << endl;
+	}
+}
