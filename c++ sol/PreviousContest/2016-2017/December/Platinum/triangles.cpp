b37bfcef82f6eee917ef5906b0d755359a5b4d15
superj6 <46981561+superj6@users.noreply.github.com>
2019-12-13 19:03:00 -0800

    Update triangles.cpp

--- c++ sol/PreviousContest/2016-2017/December/Platinum/triangles.cpp
+++ c++ sol/PreviousContest/2016-2017/December/Platinum/triangles.cpp
@@ -1,3 +1,15 @@
+/*
+	The naive solution to this is to try all triangles and then test every point to see if it is inside. This right away is
+O(n^4), however, this is not fast enough for n = 300. However, because the n bounds is relatively small, you can see that an O(n^3)
+solution would work. This means you could still iterate over all triangles in time, however, you would then have to calculate the
+number of points in a triangle in O(1). To do this, first think about how you can split counting up the triangle into counting
+something for each line. You can see that that every triangle can be confined bythe area of 1 or 2 segments on top minus the area
+of other 2 or 1 segments on the bottom. This means what you can do is precalculate the number of under each segment in O(n^3) by
+iterating over every segment and testing if every point is under it, then iterate over every triangle and set the number of points
+for that triangle equal to the number of points under the top segments minus the number of points under the bottom segments of the
+triangle. You then just have to be careful about the case when points are on a line. The solutions therefore is O(n^3) as desired.
+*/
+
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
