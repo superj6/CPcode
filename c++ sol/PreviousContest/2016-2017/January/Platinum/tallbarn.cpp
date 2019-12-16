8f9672f3b4842972d253219c8a5d394028b5b2c6
superj6 <46981561+superj6@users.noreply.github.com>
2019-12-16 10:22:41 -0600

    Update tallbarn.cpp

--- c++ sol/PreviousContest/2016-2017/January/Platinum/tallbarn.cpp
+++ c++ sol/PreviousContest/2016-2017/January/Platinum/tallbarn.cpp
@@ -1,3 +1,18 @@
+/*
+	The first thing to realize for this problem is, after allocating one cow to 
+each building, you need to greedily set the the cow to whichever building will make
+it cause the most change. To do this, if you let the current number of cows currently
+on a property a[i] be z, the change when adding one more cow is a[i] / z - a[i] / (z + 1)
+which is equal to a[i] / (z * (z + 1)). However, because k is large, you cannot just find
+the k best changes one at a time. Instead you can binary search on on what the minimum
+final change is for each property such that it is more than x. This is because you want
+to greedily choose them so they will all be as close to the minimum value as possible,
+and as the final change decreases more cows will be needed, so for the binary search
+you can see if the amount of cows used is greater than or equal to k and calculate
+the answer used so far. Lastly for the answer, you need to adjust it by adding the
+change added by the extra cows so you have the answer for exactly k.
+*/
+
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
