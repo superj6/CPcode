efed78459a9ff8d962e041caaa0c5e8162fe9bf5
superj6 <46981561+superj6@users.noreply.github.com>
2020-01-10 01:56:18 -0600

    Add files via upload

---
+++ c++ sol/USACO/2012-2013/March/Gold/necklace.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <string.h>
+using namespace std;
+#define endl '\n'
+#define pi pair<int, int>
+
+const int maxm = 1000;
+int n, m;
+string s, t;
+int dp[maxm], dp2[maxm];
+int nx[maxm][26];
+
+int main(){
+	freopen("necklace.in", "r", stdin);
+	freopen("necklace.out", "w", stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> s >> t;
+	n = s.size(), m = t.size();
+	
+	for(int i = 0; i < n; i++) s[i] -= 'a';
+	for(int i = 0; i < m; i++) t[i] -= 'a';
+	
+	for(int i = 1; i < m; i++){
+		dp[i] = dp2[i] = -n;
+		int p = nx[i - 1][t[i - 1]];
+		nx[i - 1][t[i - 1]] = i;
+		memcpy(nx[i], nx[p], sizeof(nx[p]));
+	}
+	nx[m - 1][t[m - 1]] = m;
+	
+	int ret = 0;
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
+			int c = nx[j][s[i]];
+			if(c < m){
+				dp2[c] = max(dp2[c], dp[j] + 1);
+				ret = max(ret, dp2[c]);
+			}
+		}
+		memcpy(dp, dp2, sizeof(dp2));
+	}
+	ret = n - ret;
+	
+	cout << ret << endl;
+
+	return 0;
+}
