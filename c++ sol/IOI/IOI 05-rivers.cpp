6c50c076d0f530f81c983d17a1c67a04ef74177d
superj6 <46981561+superj6@users.noreply.github.com>
2020-07-25 15:44:21 -0500

    f

---
+++ c++ sol/IOI/IOI 05-rivers.cpp
@@ -0,0 +1,70 @@
+/*
+  Dfs from root, and then from the current node during dfs after computing dp values in all subtrees I call dfs2 to compute the current nodes dp values of the minimal cost with
+each amount of nodes marked assuming it's marked. In dfs2 I try combining different amounts of marked nodes in each subtree with knapsack, then see If I should mark the current
+node. One knapsack takes O(k^2), and dfsing from each node takes O(n^2) for a total of O((nk)^2).
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define pi pair<int, int>
+#define f first
+#define s second
+#define sz(k) (k * sizeof(ll))
+
+const ll inf = 0x3f3f3f3f3f3f3f3f;
+const int mxn = 101;
+int n, k;
+ll a[mxn], d[mxn];
+ll dp[mxn][mxn], dp2[mxn][mxn];
+vector<pi> g[mxn];
+
+void dfs2(int c){
+	memset(dp2[c], inf, sz(k));
+	dp2[c][0] = a[c] * d[c];
+	for(pi i : g[c]){
+		d[i.f] = d[c] + i.s;
+		dfs2(i.f);
+		for(int j = k - 1; ~j; j--){
+			ll ret = inf;
+			for(int l = 0; l <= j; l++){
+				ret = min(ret, dp2[c][j - l] + dp2[i.f][l]);
+			}
+			dp2[c][j] = ret;
+		}
+	}
+	for(int i = 0; i < k; i++) dp2[c][i] = min(dp2[c][i], dp[c][i]);
+}
+
+void dfs(int c){
+	for(pi i : g[c]) dfs(i.f);
+	d[c] = 0;
+	memset(dp[c], inf, sz(k));
+	dfs2(c);
+	memcpy(dp[c] + 1, dp2[c], sz(k));
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> k;
+	k++;
+	
+	for(int i = 1; i <= n; i++){
+		int p, w;
+		cin >> a[i] >> p >> w;
+		g[p].push_back({i, w});
+	}
+	
+	dfs(0);
+	
+	cout << dp[0][k] << endl;
+
+	return 0;
+}
