8eb88dc8b832814379a0cdbb206361d071a291a6
superj6 <46981561+superj6@users.noreply.github.com>
2020-09-02 22:43:27 -0500

    Update POI 13-Polarization.cpp

--- c++ sol/POI/POI 13-Polarization.cpp
+++ c++ sol/POI/POI 13-Polarization.cpp
@@ -1,5 +1,14 @@
 /*
-
+	First realize you always want to root a tree and have all edges either going toward the root or going away from the root.
+This is because it is always better to multiply two numbers as large as possible than multiply combinations of different parts. Next
+realize it is always best to root at a centroid node. You can prove this by analyzing the difference between a non-centroid node and
+going to a node 1 closer to the centroid. This means the total answer will be the sum of all subtree sizes, since all edges going up
+or going down either allows each node to reach the root or the root to reach each node of the subtree, plus the product of the best
+partition of the subtree sizes of a centroid into two groups. You can find all possible partitions with knapsack in O(nsqrtn) by holding
+the multiplicity of each size and making sure you never iterate adding in the knapsack over the same area twice. The sqrtn is because
+there can be at most sqrtn distinct sizes that in total add to n. This means you can just try rerooting the tree and test for each node
+if its a centroid, and if it is, do knapsack and try all the partition products plus the dp value. Because there are at most 2 centroids
+you only run the knapsack at most twice so the time complexity is O(nsqrtn).
 */
 
 #include <iostream>
