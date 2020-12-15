4a74806f8ce3d70c2863ec4102170665799a1ab6
super-j6 <super_j@att.net>
2020-12-15 11:16:39 -0600

    Add NOIS feast.

---
+++ c++ sol/NOIS/NOI 19-feast.cpp
@@ -0,0 +1,52 @@
+/*
+	This can be solved with wqs bsearch (alien's trick). Just binary search on amount to subtract when using a
+segment, then greedily go through array holding best transition using prefix sums and take segment whenever useful,
+keeping track of how many you take, then add back amount u subtracted away.
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+#define endl '\n'
+#define ll long long
+#define pi pair<ll, ll>
+#define f first
+#define s second
+
+const int mxn = 300001;
+int n, k;
+ll a[mxn];
+pi dp[mxn];
+
+pi sol(ll x){
+	pi p = dp[0];
+	for(int i = 1; i <= n; i++){
+		dp[i] = dp[i - 1];
+		if(p.f + a[i] - x > dp[i].f) dp[i] = {p.f + a[i] - x, p.s + 1};
+		if(dp[i].f - a[i] > p.f) p = {dp[i].f - a[i], dp[i].s};
+	}
+	return dp[n];
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> k;
+	
+	for(int i = 1; i <= n; i++) cin >> a[i], a[i] += a[i - 1];
+	
+	ll l = -1, r = 0x3f3f3f3f3f3f3f3f;
+	while(r - l > 1){
+		ll mid = (l + r) / 2;
+		if(sol(mid).s <= k) r = mid; 
+		else l = mid;
+	}
+	
+	pi p = sol(r);
+	
+	cout << (p.f + p.s * r) << endl;
+
+	return 0;
+}
