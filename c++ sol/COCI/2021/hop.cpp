fa2f49f1337944620fbcd3ff9d656596ac848422
super-j6 <super_j@att.net>
2021-01-16 13:31:37 -0600

    COCI Hop AC.

--- c++ sol/COCI/2021/hop.cpp
+++ c++ sol/COCI/2021/hop.cpp
@@ -8,9 +8,9 @@ using namespace std;
 #define f first
 #define s second
 
-const int mxn = 1000, k = 3;
+const int mxn = 1000, k = 4;
 int n;
-ll a[mxn], dp[mxn][k];
+ll a[mxn];
 
 int main(){
 	ios::sync_with_stdio(0);
@@ -20,10 +20,11 @@ int main(){
 	
 	for(int i = 0; i < n; i++){
 		cin >> a[i];
+		a[i] = __lg(a[i]);
 		for(int j = 0; j < i; j++){
-			int x = min_element(dp[j], dp[j] + k) - dp[j];
-			if(!(a[i] % a[j])) dp[i][x] = max(dp[i][x], dp[j][x] + 1);
-			cout << (x + 1) << (j == i - 1 ? endl : ' ');
+			int x = 1, y = a[i], z = a[j];
+			while(y / k != z / k) x++, y /= k, z /= k;
+			cout << x << (j == i - 1 ? endl : ' ');
 		}
 	}
 	
